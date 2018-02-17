55505823ee30efe3973b15b7efad8cbad94ee081
Abhishek Birjepatil <abhishek_birjepatil@hotmail.com>
2018-02-17 13:02:02 -0800

    Init Commit

--- ClassesAndObjects/ClassesAndObjects.py
+++ ClassesAndObjects/ClassesAndObjects.py
@@ -1,1 +1,28 @@
+#!/usr/bin/python
+
+class DatabaseAccess:
+    mDbQuery = "Test";
+
+    #Constructor for the Database Class
+    def __init__(self,str):
+      self.mDbQuery = "Test Value changed from constructor"
+
+    def DisplayValue(self):
+        print self.mDbQuery;
+
+
+
+        
+mDao = DatabaseAccess("test");
+mDao.DisplayValue();
+print(hasattr(mDao,"mDbQuery"));
+
+print(getattr(mDao,"mDbQuery"));
+
+print(mDao.mDbQuery);
+
+setattr(mDao,"mDbQuery","Value Changed");
+
+pri
+
 
